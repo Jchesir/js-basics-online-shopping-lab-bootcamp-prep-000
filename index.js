34f147b5652c3f203a1ddba7260caa084cef5940
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-04-16 17:12:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ function placeOrder(cardNumber) {
   return "Sorry, we don't have a credit card on file for you."
   
  } else {
-
+var totalsums = total()
 setCart([])
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${totalsums}, which will be charged to the card ${cardNumber}.`
 }
 }
